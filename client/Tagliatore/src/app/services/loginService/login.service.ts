e8cb472b85509fd8e693f80f025efc9278eba5ee
Smogg21 <spazzaglia98@gmail.com>
2024-12-17 19:40:37 -0500

    Chat ahora usa sesión. Server ahora devuelve nombre del usuario

--- client/Tagliatore/src/app/services/loginService/login.service.ts
+++ client/Tagliatore/src/app/services/loginService/login.service.ts
@@ -13,10 +13,12 @@ export class LoginService {
   constructor(private http: HttpClient, private router: Router) { }
 
   login(usuario: string, password: string): Observable<any> {
-    return this.http.post<{ token: string, userName: string }>(this.loginUrl, { usuario, password: password }).pipe(
+    return this.http.post<{ token: string, userName: string, nombre: string }>(this.loginUrl, { usuario, password: password }).pipe(
       tap(response => {
         sessionStorage.setItem('token', response.token);
         sessionStorage.setItem('userName', response.userName);
+        sessionStorage.setItem('nombre', response.nombre);
+        console.log(response);
       })
     );
   }
@@ -24,6 +26,7 @@ export class LoginService {
   logout(): void {
     sessionStorage.removeItem('token');
     sessionStorage.removeItem('userName');
+    sessionStorage.removeItem('nombre');
     this.router.navigate(['/login']);
   }
 
@@ -33,6 +36,9 @@ export class LoginService {
 
   getUser(): string | null {
     return sessionStorage.getItem('userName');
+  }
+  getNombre(): string | null {
+    return sessionStorage.getItem('nombre');
   }
 
   isLoggedIn(): boolean {
